9ce2715ef4fd78dd6f2163c19504ad0e4264aa6e
Mohsin-khan540 <arhamk4555@gmail.com>
2025-08-10 18:36:52 +0500

    Initial Commit

---
+++ EduTainment App/EduTainment_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  EduTainment_AppApp.swift
+//  EduTainment App
+//
+//  Created by Mohsin khan on 10/08/2025.
+//
+
+import SwiftUI
+
+@main
+struct EduTainment_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ EduTainment AppTests/EduTainment_AppTests.swift
@@ -0,0 +1,17 @@
+//
+//  EduTainment_AppTests.swift
+//  EduTainment AppTests
+//
+//  Created by Mohsin khan on 10/08/2025.
+//
+
+import Testing
+@testable import EduTainment_App
+
+struct EduTainment_AppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
